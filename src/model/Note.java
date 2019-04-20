d71ff49ddaff92599abfdda663efbd3e5e74cc32
dianasaz <dinka2223@mail.ru>
2019-04-20 20:54:40 +0600

    first

---
+++ src/model/Note.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Note {
+    private String nameOfTournament;
+    private int date;
+    private String kindOfSport;
+    private String winner;
+    private int prize;
+    private int prizeOfWinner;
+
+    public
+}
